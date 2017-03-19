dffaa115e6ac2168d613dbab535ab3ccecffcf81
kavics <gyeby5@gmail.com>
2017-03-19 12:55:29 +0100

    Improved help functionality. (#4)
    * Remove read-only flags from sandbox files before deleting them. (#3)
    * Main help screen with available package names.
    * List available package names and their descriptions.
    * Package help with parameter info.

---
+++ src/SnAdmin/PackageHelpInfo.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace SenseNet.Tools.SnAdmin
+{
+    internal class PackageHelpInfo
+    {
+        public string Name { get; }
+        public string Path { get; }
+        public bool Compressed { get; }
+
+        public PackageHelpInfo(string path, bool compressed)
+        {
+            Path = path;
+            Compressed = compressed;
+            Name = compressed ? System.IO.Path.GetFileNameWithoutExtension(path) : System.IO.Path.GetFileName(path);
+        }
+
+        private XmlDocument _manifest;
+
+        private XmlDocument GetManifest()
+        {
+            if (_manifest == null)
+            {
+                var xml = new XmlDocument();
+                var manifestPath = Directory.GetFiles(Path).FirstOrDefault();
+                if (manifestPath != null)
+                    xml.Load(manifestPath);
+                _manifest = xml;
+            }
+            return _manifest;
+        }
+
+        public string GetDescription()
+        {
+            if (Compressed)
+                return "(compressed)";
+            return GetManifest()?.SelectSingleNode("/Package/Description")?.InnerXml;
+        }
+
+        public PackageParameterHelpInfo[] GetParameters()
+        {
+            var manifest = GetManifest();
+            if(manifest == null)
+                return new PackageParameterHelpInfo[0];
+
+            return manifest.SelectNodes("/Package/Parameters/Parameter")
+                .Cast<XmlElement>()
+                .Select(p => new PackageParameterHelpInfo
+                {
+                    Name = p.Attributes["name"].Value.Trim('@'),
+                    DefaultValue = p.InnerXml,
+                    Description = p.Attributes["description"]?.Value
+                })
+                .ToArray();
+        }
+    }
+
+    internal class PackageParameterHelpInfo
+    {
+        public string Name;
+        public string Description;
+        public string DefaultValue;
+    }
+}
